0ab8dccac9bb7ae5feca3f3f99b35854b77b6884
James Riley Wynne III <wynnejr@ornl.gov>
2014-11-20 15:09:55 -0500

    Tested on rhea and works! Mpi_run and finalize

--- src/rustMPI.rs
+++ src/rustMPI.rs
@@ -1,24 +1,48 @@
+#[allow(non_snake_case_functions)]
 extern crate libc;
-use self::libc::{c_int, c_float, c_double, size_t};
-use slice::raw;
+pub use self::libc::{c_int,c_float,c_double,c_char};
+use std::vec;
+use std::os;
+use std::ptr;
 
 pub fn test() {
     println!("It works!");
 }
-
+#[link(name = "pthread")]
+#[link(name = "mpi")]
+#[link(name = "osmcomp")]
+#[link(name = "rdmacm")]
+#[link(name = "ibverbs")]
+#[link(name = "rt")]
+#[link(name = "nsl")]
+#[link(name = "util")]
+#[link(name = "psm_infinipath")]
+#[link(name = "torque")]
+#[link(name = "dl")]
+#[link(name = "m")]
+#[link(name = "numa")]
+#[link(name = "rt")]
+#[link(name = "nsl")]
+#[link(name = "util")]
 extern "C" {
-    pub fn rMPI_Init(argc: &[int], argv: &[[char]char]) -> int {
-        unsafe {
-            let pargc = argc.as_ptr();
-            let pargv = argv.as_mut_prt();
-            let ret = MPI_Init(pargc, pargv);
-            return ret;
-        }
+    fn MPI_Init(argc: *const c_int, argv: *const c_char) -> int;
+    fn MPI_Finalize() -> int;
+}
+
+#[fixed_stack_segment]
+pub fn rMPI_Init() -> int {
+    unsafe {
+        let argc = ptr::null();
+        let argv = ptr::null();
+        let ret = MPI_Init(argc, argv);
+        return ret;
     }
-    pub fn rMPI_Finalize() -> int {
-        unsafe {
-            let ret = MPI_Finalize();
-            return ret;
-        }
+}
+
+#[fixed_stack_segment]
+pub fn rMPI_Finalize() -> int {
+    unsafe {
+        let ret = MPI_Finalize();
+        return ret;
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -4,6 +4,7 @@ fn main()
 {
     //Won't compile. argc and argv haven't been defined. how would i get that??
     rustMPI::test();
-    rustMPI::rMPI_Init(argc,argv);
-    rustMPI::rMPI_Finialize();
+    rustMPI::rMPI_Init();
+    println!("HelloooO");
+    rustMPI::rMPI_Finalize();
 }
