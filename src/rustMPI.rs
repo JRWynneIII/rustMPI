bd1f5edb3f78a5d5dede426f76be6117c7834277
James Riley Wynne III <wynnejr@ornl.gov>
2014-11-19 20:29:08 -0500

    Added start for definition for MPI_Init and MPI_finalize

--- src/rustMPI.rs
+++ src/rustMPI.rs
@@ -1,7 +1,24 @@
+extern crate libc;
+use self::libc::{c_int, c_float, c_double, size_t};
+use slice::raw;
+
 pub fn test() {
     println!("It works!");
 }
 
 extern "C" {
-    pub fn MPI_Send();
+    pub fn rMPI_Init(argc: &[int], argv: &[[char]char]) -> int {
+        unsafe {
+            let pargc = argc.as_ptr();
+            let pargv = argv.as_mut_prt();
+            let ret = MPI_Init(pargc, pargv);
+            return ret;
+        }
+    }
+    pub fn rMPI_Finalize() -> int {
+        unsafe {
+            let ret = MPI_Finalize();
+            return ret;
+        }
+    }
 }
--- src/test.rs
+++ src/test.rs
@@ -2,5 +2,8 @@
 mod rustMPI;
 fn main()
 {
+    //Won't compile. argc and argv haven't been defined. how would i get that??
     rustMPI::test();
+    rustMPI::rMPI_Init(argc,argv);
+    rustMPI::rMPI_Finialize();
 }
